4d2161d62ce7118a7d7d6278d0a5f2a0e4923c30
Chris Gurung <chris.gurung@waterflow.technology>
2024-07-10 15:45:11 +0545

    feat: added create, update, delete and generate file routes

---
+++ routes/queueRoutes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router= express.Router();
+const {createNewQueue,updateQueue,deleteQueue,createConfigFile}= require('../controllers/queueController');
+const { validateCreate, validateUpdateQueue, validateDelete } = require('../middleware/validator');
+
+router.get('/',(req,res)=>{
+    res.send('WELCOME TO QUEUE CONFIG GENERATOR')
+})
+
+router.post('/create',validateCreate, async (req, res) => {
+    try {
+        const queue = req.body
+        const response = await createNewQueue(queue)
+        console.log(response)
+        res.send(response)
+    }
+    catch (err) {
+        console.log('ERR::::::', err)
+    }
+
+})
+router.patch('/update',validateUpdateQueue, async (req, res) => {
+    try {
+        const { newname, ...queue } = req.body
+        const response = await updateQueue(newname, queue)
+        res.send(response)
+    }
+    catch (err) {
+        console.log('ERR::::::', err)
+        res.status(400).json({error:"Internal server error"})
+    }
+
+})
+router.delete('/delete',validateDelete, async (req, res) => {
+    try {
+        const {queue_name} = req.body
+        const response = await deleteQueue(queue_name)
+        res.send(response)
+    }
+    catch (err) {
+        console.log('ERR::::::', err)
+        throw new Error(err)
+    }
+
+})
+router.post('/generate', async (req, res) => {
+    try {
+        res.send(await createConfigFile())
+    }
+    catch (err) {
+        throw new Error(err)
+    }
+})
+
+module.exports = router; 
