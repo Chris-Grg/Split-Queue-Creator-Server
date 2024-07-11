e875b884bb65da1a14ae17c5356547dd85847b19
Chris Gurung <chris.gurung@waterflow.technology>
2024-07-11 10:19:02 +0545

    chore: added createConfigFile function after each member crud operation

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,17 +1,21 @@
 const db = require('../db/connection');
+const queueController = require('./queueController');
 
 const addMembers =(queue, name)=>{
     return new Promise((resolve,reject)=>{
         const selectQuery =`SELECT * from members WHERE members.queue = ? AND members.name = ?`
         db.query(selectQuery, [queue,name],(err,res)=>{
-            if(err) return reject(err);
+            if(err)  reject(err);
             if(res.length>0){
-                return reject(new Error('Member already exists'));
+                 reject(new Error('Member already exists'));
             }
             const insertQuery = `INSERT INTO members (queue,name) VALUES (?,?)`
             db.query(insertQuery, [queue,name],(err,res)=>{
-                if(err) return reject(err);
-                resolve(res);
+                if(err) reject(err);
+                else{
+                    queueController.createConfigFile()
+                    resolve('member added')
+                };
             })
         })
     })
@@ -22,12 +26,14 @@ const selectMemberQuery = `SELECT * from members WHERE members.queue = ? AND mem
     db.query(selectMemberQuery, [queue,name],(err,res)=>{
         if(err) return reject(err);
         if(res.length===0){
-            return reject(new Error('Member not found'));
+         reject(new Error('Member not found'));
         }
         const deleteQuery = `DELETE FROM members WHERE members.queue =? AND members.name =?`
         db.query(deleteQuery, [queue,name],(err,res)=>{
             if(err) return reject(err);
-            resolve(res);
+            else{queueController.createConfigFile()
+
+            resolve(`name: ${name} queue: ${queue} deleted`);}
         })
     })
 
@@ -47,10 +53,11 @@ return new Promise((resolve,reject)=>{
             if(res.length>0){
                 return reject(new Error('Member already exists in new queue'));
             }
-        const updateQuery = `UPDATE members SET members.name =? WHERE members.queue =? AND members.name =?`
-        db.query(updateQuery, [newName,queue,name],(err,res)=>{
+        const updateQuery = `UPDATE members SET members.name =?, members.queue=? WHERE members.queue =? AND members.name =?`
+        db.query(updateQuery, [newname,newqueue,queue,name],(err,res)=>{
             if(err) return reject(err);
-            resolve(res);
+            else{queueController.createConfigFile()
+            resolve(`Queue: ${queue} Name: ${name} updated`);}
         })
     })
     })
