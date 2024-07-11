3fe43e9ee53597bd07260f38a97c83a0cb9e40c4
Chris Gurung <chris.gurung@waterflow.technology>
2024-07-11 17:57:34 +0545

    chore: added ami status checks logs and swapped command from dialplan reload to queue reload all

--- controllers/amiController.js
+++ controllers/amiController.js
@@ -5,12 +5,20 @@ var ami = new require('asterisk-manager')(
     process.env.ASTERISK_PASSWORD,
      true);
 ami.keepConnected();
-
+ami.on('connect', () => {
+  console.log('Connected to Asterisk AMI');
+});
+ami.on('disconnect', () => {
+  console.log('Disconnected from Asterisk AMI');
+});
+ami.on('reconnection', () => {
+  console.log('Reconnecting to Asterisk AMI');
+});
 const dialplanReload=async()=>{
     return new Promise((resolve, reject) => {
       ami.action({
         action: "Command",
-        command: "dialplan reload"
+        command: "queue reload all"
       }, (err, res) => {
         if (err) {
           reject(err)
