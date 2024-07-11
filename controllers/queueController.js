fa31f6e80f3e25922cc83221a125355c0143de94
Chris Gurung <chris.gurung@waterflow.technology>
2024-07-11 17:59:57 +0545

    chore: changed some promise resolves to reject with error

--- controllers/queueController.js
+++ controllers/queueController.js
@@ -8,17 +8,16 @@ const createNewQueue = async (queue) => {
         const selectQuery = 'SELECT queue_name FROM queues WHERE queue_name = ?'
         db.query(selectQuery, [queue.queue_name], async (err, res) => {
             if (err) {
-                reject(err)
+                reject(new Error(err))
             }
             else if (res.length > 0) {
-                // console.log(`Queue ${queue.queue_name} already exists.`)
-                resolve(`Queue ${queue.queue_name} already exists.`)
-            } else {
+                reject(new Error(`Queue ${queue.queue_name} already exists.`))
+            } 
+            else {
                 const insertQuery = 'INSERT INTO queues (queue_name, strategy, ringinuse, timeout, wrapuptime) VALUES (?, ?, ?, ?, ?)';
                 db.query(insertQuery, [queue.queue_name, queue.strategy, queue.ringinuse, queue.timeout, queue.wrapuptime], (err, result) => {
-                    if (err) reject(err)
+                    if (err) reject(new Error(err))
                     else {
-                        console.log(`New queue added successfully with ID: ${result.insertId}`)
                         createConfigFile()
                         resolve(`New queue added successfully with ID: ${result.insertId}`)
                     }
@@ -32,17 +31,24 @@ const deleteQueue = async (queue_name) => {
     return new Promise((resolve, reject) => {
         const selectQuery = 'SELECT queue_name FROM queues WHERE queue_name = ?'
         db.query(selectQuery, [queue_name], async(err,res)=>{
-if(err) reject(err)
+if(err) reject(new Error(err))
 else if (res.length===0){
-resolve(`Queue ${queue_name} doesn't exist`)
+reject(new Error(`Queue ${queue_name} doesn't exist`))
 }
 else{
         const deleteQuery = `DELETE FROM queues WHERE queue_name= ?`
        db.query(deleteQuery,[queue_name], (err, result) => {
-            if (err) reject(err)
+            if (err) reject(new Error(err.message))
             else {
                 createConfigFile()
-                resolve(`${queue_name} queue removed successfully`)
+                 .then(()=>{
+
+                     resolve(`${queue_name} queue removed successfully`)
+                 })
+                 .catch(configErr => {
+                     console.error('Error creating config file:', configErr);
+                     reject(new Error('Member added but failed to update config'));
+                 });
                 }
 
               })
@@ -55,22 +61,27 @@ const updateQueue = async (newname, queue) => {
     return new Promise((resolve, reject) => {
         const selectQuery = 'SELECT queue_name FROM queues WHERE queue_name = ?'
         db.query(selectQuery,[queue.queue_name], async (err, res) => {
-            if (err) reject(err)
+            if (err) reject(new Error(err))
             else if (res.length === 0) {
-                resolve(`Queue ${queue.queue_name} doesn't exist.`)
+                reject(new Error(`Queue ${queue.queue_name} doesn't exist.`))
             }
             db.query(selectQuery,[newname], async (err, res) => {
-                if (err) reject(err)
+                if (err) reject(new Error(err))
                 else if (res.length > 0) {
-                    resolve(`Queue name ${newname} already exists`)
+                    reject(new Error(`Queue name ${newname} already exists`))
                 }
                 else {
                     const updateQuery = `UPDATE queues SET queue_name=?, strategy=?,ringinuse=?,timeout=?, wrapuptime=? WHERE queue_name=?`;
                     db.query(updateQuery,[newname,queue.strategy,queue.ringinuse,queue.timeout,queue.wrapuptime,queue.queue_name], (err, result) => {
-                        if (err) reject(err)
+                        if (err) reject(new Error(err))
                         else {
                             createConfigFile()
-                            resolve(`Queue updated successfully`)
+                                .then(() => resolve('Queue updated successfully'))
+                                .catch(configErr => {
+                                    console.error('Error creating config file:', configErr);
+                                    reject(new Error('Member added but failed to update config'));
+                                });
+    
                         }
                     })
                 }
@@ -84,30 +95,30 @@ const createConfigFile = async () => {
         const selectQuery = 'SELECT queue_name, strategy, ringinuse, timeout, wrapuptime FROM queues'
         db.query(selectQuery, (err, results) => {
             if (err) {
-                reject(`ERROR:::::::${err}`);
+                reject(new Error(`ERROR:::::::${err}`));
             } else {
                 let configFileContent = '';
 
                 const queuePromises = results.map(row => {
-                    return new Promise((queueResolve, queueReject) => {
+                    return new Promise((resolve, reject) => {
                         configFileContent += `
 [${row.queue_name}]
 strategy=${row.strategy}
-ringinuse=${row.ringinuse}
+ringinuse=${row.ringinuse?'yes':'no'}
 timeout=${row.timeout}
 wrapuptime=${row.wrapuptime} \n
 `;
                         const selectMemberQuery= `SELECT name from members where members.queue=?`
                         db.query(selectMemberQuery,[row.queue_name], (err, memberResults) => {
                             if (err) {
-                                queueReject(err);
+                                reject(new Error(err));
                             } else {
                                 if (memberResults.length > 0) {
                                     memberResults.forEach(memberRow => {
                                         configFileContent += `member=> PJSIP/${memberRow.name}\n`;
                                     });
                                 }
-                                queueResolve();
+                                resolve();
                             }
                         });
                     });
@@ -117,16 +128,19 @@ wrapuptime=${row.wrapuptime} \n
                     .then(() => {
                         fs.writeFile(process.env.CONFIG_FILE_OUTPUT_LOCATION, configFileContent, (err) => {
                             if (err) {
-                                console.error('Error writing to queue.conf:', err);
-                                reject(err);
+                                reject(new Error(err));
                             } else {
-                                ami.dialplanReload();  // reload the dialplan after creating the queue.conf file to apply the changes.
-                                resolve('queue.conf file created and dialplan reloaded successfully');
+                                ami.dialplanReload()
+                                    .then(
+                                        ()=>resolve('queue.conf file created and dialplan reloaded successfully')
+
+                                    )
+                                    .catch(err=> reject(new Error("failed to reload dialplan")));
                             }
                         });
                     })
                     .catch(error => {
-                        reject(`ERROR:::::::${error}`);
+                        reject(new Error(`ERROR:::::::${error}`));
                     });
             }
         });
